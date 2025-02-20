ee7c1720ded8c92df1823814d729a4c81ce0d55f
Pietro Rustici <p.rustici93@gmail.com>
2025-02-20 23:09:01 +0100

    add missing imports

--- ESP32CamAI/Config.cpp
+++ ESP32CamAI/Config.cpp
@@ -10,7 +10,7 @@ const char* OpenAIConfig::API_KEY = "*****";
 const char* OpenAIConfig::MODEL = "gpt-4o-mini";
 const int OpenAIConfig::MAX_CALLS = 3;
 const int OpenAIConfig::MAX_TOKENS = 2000;
-const char* OpenAIConfig::BASE_PROMPT = "You are an automata with a goal and a list of actions that you can execute to achieve that goal. \\\n"
+const char* OpenAIConfig::BASE_PROMPT = "You are an automata with a goal and a list of actions that you can execute to achieve that goal. \\n"
                                       "On each iteration you will receive an image, you analyze the image and return the action that is most probable to make you achieve your goal. \\n"
                                       "Return the action in plain text, without any additional symbols or explanation. \\n";
 const char* OpenAIConfig::GOAL = "Goal: turn on the light if there are people in the room, else turn the light off. \\n";
