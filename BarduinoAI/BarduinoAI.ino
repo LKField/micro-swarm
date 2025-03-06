3ed2cfad72d80e3692823e545954e962c99a8815
Flax96-BIT <flavio.grimaldi@students.iaac.net>
2025-03-06 17:22:19 +0100

    vibration sensor action the buzzer

--- BarduinoAI/BarduinoAI.ino
+++ BarduinoAI/BarduinoAI.ino
@@ -115,6 +115,7 @@ void setupAI() {
   
   // Initialize AI controller
   aiController = new AIController(client);
+
 }
 
 // ---------------------------------------AI RESPONSE---------------------------------------
